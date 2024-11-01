798a0dccc80ed906454cd4205277bbd55a2c133d
Marquis <mac@Apples-MacBook-Pro.local>
2024-11-01 05:28:21 -0700

    update

--- pages/index.vue
+++ pages/index.vue
@@ -1,22 +1,18 @@
-<template>
-  <main>
-    <hero-section id="about-section" />
-    <hero-feature id="services-section" />
-    <feature-one id="services-section" />
-    <feature-two id="services-section" />
-    <feature-three id="services-section" />
-    <feature-four id="services-section" />
-    <stat-section />
-    <updated-testimonials class="" />
-    <cta-section />
-    <image-carousel id="projects"></image-carousel>
-    <frequently-asked-questions />
-    <contact-section id="contact-section" />
-    <footer-section />
-  </main>
-</template>
-
 <script setup lang="ts">
-const openSidebar = ref(false);
-const speed = ref(10);
+import { onMounted } from 'vue'
+import { useRouter } from 'vue-router'
+
+const router = useRouter()
+
+definePageMeta({
+  title: 'Home Page',
+  layout: 'authentication',
+  meta: [
+    { name: 'description', content: 'This is the home page of my Nuxt 3 app.' }
+  ]
+})
+
+onMounted(() => {
+  router.push('/auth')
+})
 </script>
